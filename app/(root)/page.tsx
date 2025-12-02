d471d2a3010b29da9bc8acfaed115e6b6e772ec1
harshbajani <harshbajani7@gmail.com>
2025-12-02 11:52:06 +0530

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import ScrollSmoothProvider from "@/components/ScrollSmoothProvider";
+
+export default function Home() {
+  return (
+    <ScrollSmoothProvider>
+      <div className="bg-muted min-h-screen">hi</div>
+    </ScrollSmoothProvider>
+  );
+}
